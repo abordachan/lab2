16ee08ee6ad9d47a599f2809719708b0ea6765ed
abordachan <andrea.bordachan@gmail.com>
2026-05-01 02:57:51 -0400

    typo 3

--- script.js
+++ script.js
@@ -28,6 +28,13 @@ map.on('load', () => {
     "source": "water-data",
     "id": "drinking-fountains",
     // "slot": "middle",
+    "filter":[
+      "match",
+        ["get", "type"],
+        "Dog Fountain",
+        false,
+        true
+      ],
     "paint": {
       "circle-radius": 4,
       "circle-color": "blue",
