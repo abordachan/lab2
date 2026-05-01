023b5d4fbe8262ebf967c425a2d2fccabff8f540
abordachan <andrea.bordachan@gmail.com>
2026-05-01 03:04:50 -0400

    slot test

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ map.on('load', () => {
     "type": "circle",
     "source": "water-data",
     "id": "drinking-fountains",
-    // "slot": "middle",
+    "slot": "middle",
     "filter":[
       "match",
         ["get", "type"],
@@ -51,7 +51,7 @@ map.on('load', () => {
     "source": "court-data",
     "id": "tennis-courts",
     // slot set to top so that tennis courts are above drinking fountains on map
-    // "slot": "top", 
+    "slot": "top", 
     "paint": {
       "circle-radius": 5,
       "circle-color": [
@@ -72,7 +72,7 @@ map.on('load', () => {
     "id": "court-labels",
     "type": "symbol",
     "source": "court-data",
-    // "slot": "top",
+    "slot": "top",
     "layout": {
       "text-field": [
         "to-string",
